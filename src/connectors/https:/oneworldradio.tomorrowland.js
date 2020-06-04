625a706b0b7be2849f6abf4134f37134ff961adb
Yannick <yannick9906@users.noreply.github.com>
2020-06-04 10:53:39 +0200

    Delete oneworldradio.tomorrowland.js

--- src/connectors/https:/oneworldradio.tomorrowland.js
+++
@@ -1,9 +0,0 @@
-Connector.playerSelector = '.player .container-fluid';
-
-Connector.artistSelector = '.track-metadata .track-info small';
-
-Connector.trackSelector = '.track-metadata .track-info small';
-
-Connector.trackArtSelector = '.track-metadata .track-image';
-
-Connector.pauseButtonSelector = '.player-controls button';
