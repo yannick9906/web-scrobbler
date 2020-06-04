a1634a2acc09c57612395f118f3f091f530fd342
Yannick <yannick9906@users.noreply.github.com>
2020-06-04 10:54:10 +0200

    Create oneworldradio.tomorrowland.js

---
+++ src/connectors/oneworldradio.tomorrowland.js
@@ -0,0 +1,10 @@
+
+Connector.playerSelector = '.player .container-fluid';
+
+Connector.artistSelector = '.track-metadata .track-info small';
+
+Connector.trackSelector = '.track-metadata .track-info small';
+
+Connector.trackArtSelector = '.track-metadata .track-image';
+
+Connector.pauseButtonSelector = '.player-controls button';
